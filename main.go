48a0cea73de9b0e3b229c40f25a632d6bb31ba54
Hekmon <edouardhur@gmail.com>
2019-04-30 13:54:15 +0200

    update logger lib and add systemd pre support

--- main.go
+++ main.go
@@ -8,46 +8,63 @@ import (
 	"github.com/gregdel/pushover"
 	"github.com/hekmon/hllogger"
 	"github.com/hekmon/transmissionrpc"
+	systemd "github.com/iguanesolutions/go-systemd"
 )
 
-var logger *hllogger.HlLogger
-var transmission *transmissionrpc.Client
-var conf *config
-var pushoverApp *pushover.Pushover
-var pushoverDest *pushover.Recipient
-var butlerRun sync.Mutex
+var (
+	logger       *hllogger.HlLogger
+	transmission *transmissionrpc.Client
+	conf         *config
+	sysd         *systemd.Notifier
+	pushoverApp  *pushover.Pushover
+	pushoverDest *pushover.Recipient
+	butlerRun    sync.Mutex
+)
 
 func main() {
 	// Parse flags
 	logLevelFlag := flag.Int("loglevel", 1, "Set loglevel: Debug(0) Info(1) Warning(2) Error(3) Fatal(4). Default Info.")
 	confFile := flag.String("conf", "config.json", "Relative or absolute path to the json configuration file")
 	flag.Parse()
+
+	// Init systemd controller
+	var err error
+	if sysd, err = systemd.NewNotifier(); err != nil {
+		logger.Warningf("[Main] can't start systemd notifier, systemd functions won't be enabled: %v\n", err)
+	}
+
 	// Init logger
+	var ll hllogger.LogLevel
 	switch *logLevelFlag {
 	case 0:
-		logger = hllogger.New(os.Stdout, "", hllogger.Debug, 0)
+		ll = hllogger.Debug
 	case 1:
-		logger = hllogger.New(os.Stdout, "", hllogger.Info, 0)
+		ll = hllogger.Info
 	case 2:
-		logger = hllogger.New(os.Stdout, "", hllogger.Warning, 0)
+		ll = hllogger.Warning
 	case 3:
-		logger = hllogger.New(os.Stdout, "", hllogger.Error, 0)
+		ll = hllogger.Error
 	case 4:
-		logger = hllogger.New(os.Stdout, "", hllogger.Fatal, 0)
+		ll = hllogger.Fatal
 	default:
-		logger = hllogger.New(os.Stdout, "", hllogger.Info, 0)
+		ll = hllogger.Info
 	}
+	logger = hllogger.New(os.Stderr, &hllogger.Config{
+		LogLevel:              ll,
+		SystemdJournaldCompat: sysd != nil,
+	})
 	logger.Output(" ")
 	logger.Output(" • Transmission Butler •")
 	logger.Output("      ヽ(　￣д￣)ノ")
 	logger.Output(" ")
+
 	// Load config
-	var err error
 	logger.Info("[Main] Loading configuration")
 	if conf, err = getConfig(*confFile); err != nil {
 		logger.Fatalf(1, "can't load config: %v", err)
 	}
 	logger.Debugf("[Main] Loaded configuration:\n%+v", conf)
+
 	// Init pushover if enabled
 	if conf.isPushoverEnabled() {
 		pushoverApp = pushover.New(*conf.Pushover.AppKey)
@@ -61,6 +78,7 @@ func main() {
 			}
 		}
 	}
+
 	// Init transmission client
 	transmission, err = transmissionrpc.New(conf.Server.Host, conf.Server.User, conf.Server.Password,
 		&transmissionrpc.AdvancedConfig{
@@ -83,19 +101,23 @@ func main() {
 				serverVersion, transmissionrpc.RPCVersion, serverMinimumVersion)
 		}
 	}
+
 	// Start butler
 	stopSignal := make(chan struct{})
 	var wg sync.WaitGroup
 	wg.Add(1)
 	logger.Info("[Main] Starting butler")
 	go butler(stopSignal, &wg)
+
 	// Handles system signals properly
 	var mainStop sync.Mutex
 	mainStop.Lock()
 	logger.Debug("[Main] Starting signal handling goroutine")
 	go handleSignals(stopSignal, &wg, &mainStop)
+
 	// Wait butler's clean stop before exiting main goroutine
 	mainStop.Lock()
+
 	// Lock the butler run in case the worker is done but not a forced run issued by USR1
 	butlerRun.Lock()
 	logger.Info("[Main] Exiting")
